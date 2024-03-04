f7b8d95e52754480547bccd87f39f5a4253b7d43
massaaki <mmassaaki@hotmail.com>
2024-03-03 21:31:12 -0300

    classes

---
+++ br/com/buscador/client/Main.java
@@ -0,0 +1,17 @@
+package br.com.buscador.client;
+import br.com.buscador.dominio.*;
+import br.com.buscador.servico.*;
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws Exception{
+        System.out.print("Informe seu CEP: ");
+        String cep = new Scanner(System.in).nextLine();
+        Endereco endereco = ServicoCep.buscaEnderecoPelo(cep);
+
+        System.out.println("Logradouro: " + endereco.getLogradouro());
+        System.out.println("Bairro: " + endereco.getBairro());
+        System.out.println("Localidade: " + endereco.getLocalidade());
+    }
+
+}
